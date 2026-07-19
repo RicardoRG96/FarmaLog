812e9fed22cd3d98afcfaee3cf5351978c7102ff
RicardoRG96 <ricardoretamal10@gmail.com>
2026-07-19 07:42:23 -0400

    add: DocumentoVentaCenabastInvalidoException

---
+++ src/FarmaLog.Nucleo.Domain/Solicitudes/Exceptions/DocumentoVentaCenabastInvalidoException.cs
@@ -0,0 +1,10 @@
+﻿using FarmaLog.Nucleo.Domain.Common;
+
+namespace FarmaLog.Nucleo.Domain.Solicitudes.Exceptions
+{
+    public class DocumentoVentaCenabastInvalidoException : DomainException
+    {
+        public DocumentoVentaCenabastInvalidoException(string message) 
+            : base(message) {}
+    }
+}
