50a2b9ad962fc105672523abec8ae140f6f23f15
RicardoRG96 <ricardoretamal10@gmail.com>
2026-07-19 09:47:03 -0400

    add: first test to CuentaClienteTests

---
+++ tests/FarmaLog.Nucleo.Domain.Tests/Solicitudes/CuentaClienteTests.cs
@@ -0,0 +1,20 @@
+﻿namespace FarmaLog.Nucleo.Domain.Tests.Solicitudes
+{
+    [TestClass]
+    public class CuentaClienteTests
+    {
+        [TestMethod]
+        [DataRow("BI")]
+        [DataRow("A1")]
+        [DataRow("1")]
+        public void CuentaCliente_ShouldThrow_When_CodigoLaboratorioIsInvalid(string codigoLaboratorio)
+        {
+            //Arrange
+            string formatedCuentaCliente = $"{codigoLaboratorio}-0778903671";
+
+            //Act + Assert
+            Assert.ThrowsExactly<CuentaClienteInvalidaException>(
+                () => CuentaCliente.Create(formatedCuentaCliente));
+        }
+    }
+}
