69d4e0a3d6eb25276feb46a3fc6d98dcad8d9d79
RicardoRG96 <ricardoretamal10@gmail.com>
2026-07-19 09:53:18 -0400

    add: CuentaCliente

---
+++ src/FarmaLog.Nucleo.Domain/Solicitudes/CuentaCliente.cs
@@ -0,0 +1,18 @@
+﻿namespace FarmaLog.Nucleo.Domain.Solicitudes
+{
+    public sealed record CuentaCliente
+    {
+        public CodigoLaboratorio? CodigoLaboratorio { get; set; }
+        public string Cuenta { get; }
+
+        private CuentaCliente(string cuenta)
+        {
+            Cuenta = cuenta;
+        }
+
+        public static CuentaCliente Create(string cuenta)
+        {
+            return new CuentaCliente(cuenta);
+        }
+    }
+}
