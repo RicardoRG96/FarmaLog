2122c778ddd07a6ecc2833ea982b180697807823
RicardoRG96 <ricardoretamal10@gmail.com>
2026-07-20 16:22:56 -0400

    add: VO DireccionDespacho

---
+++ src/FarmaLog.Nucleo.Domain/Solicitudes/DireccionDespacho.cs
@@ -0,0 +1,19 @@
+﻿namespace FarmaLog.Nucleo.Domain.Solicitudes
+{
+    public sealed record DireccionDespacho
+    {
+        public CodigoLaboratorio Codigo { get; }
+        public string Direccion { get; }
+
+        private DireccionDespacho(CodigoLaboratorio codigo, string direccion)
+        {
+            Codigo = codigo;
+            Direccion = direccion;
+        }
+
+        public static DireccionDespacho Create(string direccion)
+        {
+            return new DireccionDespacho(CodigoLaboratorio.Create("23"), "23-778903671D1");
+        }
+    }
+}
