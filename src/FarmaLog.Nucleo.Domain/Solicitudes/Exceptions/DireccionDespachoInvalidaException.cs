9eab7b74fd76381b650603e13ea8e257a7be73cb
RicardoRG96 <ricardoretamal10@gmail.com>
2026-07-20 16:23:38 -0400

    add: DireccionDespachoInvalidaException

---
+++ src/FarmaLog.Nucleo.Domain/Solicitudes/Exceptions/DireccionDespachoInvalidaException.cs
@@ -0,0 +1,10 @@
+﻿using FarmaLog.Nucleo.Domain.Common;
+
+namespace FarmaLog.Nucleo.Domain.Solicitudes.Exceptions
+{
+    public class DireccionDespachoInvalidaException : DomainException
+    {
+        public DireccionDespachoInvalidaException(string message) 
+            : base(message) {}
+    }
+}
