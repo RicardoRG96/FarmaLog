efcba7e10b5c1f5f2b129d3eb9f87a7aaf56a63d
RicardoRG96 <ricardoretamal10@gmail.com>
2026-07-20 16:19:51 -0400

    add: first test to DireccionDespachoTests

---
+++ tests/FarmaLog.Nucleo.Domain.Tests/Solicitudes/DireccionDespachoTests.cs
@@ -0,0 +1,24 @@
+﻿using FarmaLog.Nucleo.Domain.Solicitudes;
+using FarmaLog.Nucleo.Domain.Solicitudes.Exceptions;
+
+namespace FarmaLog.Nucleo.Domain.Tests.Solicitudes
+{
+    [TestClass]
+    public class DireccionDespachoTests
+    {
+        [TestMethod]
+        [DataRow("BI")]
+        [DataRow("A1")]
+        [DataRow("1")]
+        public void DireccionDespacho_ShouldThrow_When_CodigoLaboratorioIsInvalid(
+            string codigoLaboratorio)
+        {
+            //Arrange
+            string formatedDireccionDespacho = $"{codigoLaboratorio}-778903671D1";
+
+            //Act + Assert
+            Assert.ThrowsExactly<DireccionDespachoInvalidaException>(
+                () => DireccionDespacho.Create(formatedDireccionDespacho));
+        }
+    }
+}
