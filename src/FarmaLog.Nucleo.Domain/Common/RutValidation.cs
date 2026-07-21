3d3672ae50b538b3489dc66903cfd9d2f3342312
RicardoRG96 <ricardoretamal10@gmail.com>
2026-07-21 07:28:01 -0400

    add: RutValidation static class

---
+++ src/FarmaLog.Nucleo.Domain/Common/RutValidation.cs
@@ -0,0 +1,23 @@
+﻿using FarmaLog.Nucleo.Domain.Solicitudes.Exceptions;
+
+namespace FarmaLog.Nucleo.Domain.Common
+{
+    public static class RutValidation
+    {
+        public static bool IsValidRutFormat(string rut)
+        {
+            string rutPortionWithoutDigitoVerificador = rut[..^1];
+
+            bool rutPortionWithoutDigitoVerificadorContainsOnlyDigits =
+                rutPortionWithoutDigitoVerificador.All(x => Char.IsAsciiDigit(x));
+
+            char digitoVerificador = rut[rut.Length - 1];
+
+            bool isAValidDigitoVerificador = char.IsAsciiDigit(digitoVerificador) ||
+                digitoVerificador == 'k';
+
+            return !rutPortionWithoutDigitoVerificadorContainsOnlyDigits ||
+                !isAValidDigitoVerificador;
+        }
+    }
+}
