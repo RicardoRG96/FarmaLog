c2ad31b322546bdab1fe8b9bdfeabebfcfda847f
RicardoRG96 <ricardoretamal10@gmail.com>
2026-07-23 06:23:21 -0400

    add: first test tipo orden venta

---
+++ tests/FarmaLog.Nucleo.Domain.Tests/Solicitudes/TipoOrdenVentaTests.cs
@@ -0,0 +1,14 @@
+﻿namespace FarmaLog.Nucleo.Domain.Tests.Solicitudes
+{
+    [TestClass]
+    public class TipoOrdenVentaTests
+    {
+        [TestMethod]
+        public void TipoOrdenVenta_ShouldThrow_When_CodigoLaboratorioIsInvalid()
+        {
+            //Act + Assert
+            Assert.ThrowsExactly<TipoOrdenVentaInvalidoException>(
+                () => TipoOrdenVenta.Create("BI"));
+        }
+    }
+}
