57f1584fa9420a6beb861cfe8631fa799635290d
RicardoRG96 <ricardoretamal10@gmail.com>
2026-07-23 06:26:46 -0400

    add: TipoOrdenVenta record class

---
+++ src/FarmaLog.Nucleo.Domain/Solicitudes/TipoOrdenVenta.cs
@@ -0,0 +1,20 @@
+﻿namespace FarmaLog.Nucleo.Domain.Solicitudes
+{
+    public sealed record TipoOrdenVenta
+    {
+        public CodigoLaboratorio Codigo { get; }
+        public string Tipo { get; }
+
+        private TipoOrdenVenta(CodigoLaboratorio codigo, string tipo)
+        {
+            Codigo = codigo;
+            Tipo = tipo;
+        }
+
+        public static TipoOrdenVenta Create(string tipoOrden)
+        {
+            return new TipoOrdenVenta(
+                CodigoLaboratorio.Create("23"), "23F1");
+        }
+    }
+}
