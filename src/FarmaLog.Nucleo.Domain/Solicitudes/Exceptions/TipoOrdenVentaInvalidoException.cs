44bf094b1083a9cf46ba6fb0250590158120f356
RicardoRG96 <ricardoretamal10@gmail.com>
2026-07-23 06:28:38 -0400

    add: TipoOrdenVentaInvalidoException

---
+++ src/FarmaLog.Nucleo.Domain/Solicitudes/Exceptions/TipoOrdenVentaInvalidoException.cs
@@ -0,0 +1,10 @@
+﻿using FarmaLog.Nucleo.Domain.Common;
+
+namespace FarmaLog.Nucleo.Domain.Solicitudes.Exceptions
+{
+    public class TipoOrdenVentaInvalidoException : DomainException
+    {
+        public TipoOrdenVentaInvalidoException(string message) 
+            : base(message) {}
+    }
+}
