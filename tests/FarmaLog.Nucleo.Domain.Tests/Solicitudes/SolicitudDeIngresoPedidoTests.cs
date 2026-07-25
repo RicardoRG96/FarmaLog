561d0515dce24da3eedc6505c2f4b6073867ccf0
RicardoRG96 <ricardoretamal10@gmail.com>
2026-07-25 13:00:58 -0400

    add: tests SolicitudDeIngresoPedido construct with valid cenabast info

--- tests/FarmaLog.Nucleo.Domain.Tests/Solicitudes/SolicitudDeIngresoPedidoTests.cs
+++ tests/FarmaLog.Nucleo.Domain.Tests/Solicitudes/SolicitudDeIngresoPedidoTests.cs
@@ -24,5 +24,31 @@ namespace FarmaLog.Nucleo.Domain.Tests.Solicitudes
             Assert.ThrowsExactly<SolicitudIncoherenteRespectoACenabastException>(
                 () => SolicitudDeIngresoPedido.Create(false, documentoVentaCenabast));
         }
+
+        [TestMethod]
+        public void SolicitudDeIngresoPedido_ShouldConstruct_When_EsCenabastIsTrueAndDocumentoVentaCenabastIsPresent()
+        {
+            //Arrange
+            string expected = "123456789";
+            DocumentoVentaCenabast documentoVentaCenabast = DocumentoVentaCenabast.Create("123456789");
+
+            //Act
+            SolicitudDeIngresoPedido solicitudDeIngresoPedido = SolicitudDeIngresoPedido.Create(
+                true, documentoVentaCenabast);
+
+            //Assert
+            Assert.AreEqual(expected, solicitudDeIngresoPedido.DocumentoVentaCenabast!.Documento);
+        }
+
+        [TestMethod]
+        public void SolicitudDeIngresoPedido_ShouldConstruct_When_EsCenabastIsFalseAndDocumentoVentaCenabastIsNull()
+        {
+            //Act
+            SolicitudDeIngresoPedido solicitudDeIngresoPedido = SolicitudDeIngresoPedido.Create(
+                false, null);
+
+            //Assert
+            Assert.IsNotNull(solicitudDeIngresoPedido);
+        }
     }
 }
