09b69f49599fd11144285a2ce4667d710ad16c4c
RicardoRG96 <ricardoretamal10@gmail.com>
2026-07-25 11:33:47 -0400

    add: SolicitudDeIngresoPedido

---
+++ src/FarmaLog.Nucleo.Domain/Solicitudes/SolicitudDeIngresoPedido.cs
@@ -0,0 +1,21 @@
+﻿namespace FarmaLog.Nucleo.Domain.Solicitudes
+{
+    public sealed class SolicitudDeIngresoPedido
+    {
+        public bool EsCenabast { get; set; }
+        public DocumentoVentaCenabast? DVCenabast { get; set; }
+
+        private SolicitudDeIngresoPedido(
+            bool esCenabast, DocumentoVentaCenabast documentoVentaCenabast)
+        {
+            EsCenabast = esCenabast;
+            DVCenabast = documentoVentaCenabast;
+        }
+
+        public static SolicitudDeIngresoPedido Create(
+            bool esCenabast, DocumentoVentaCenabast documentoVentaCenabast)
+        {
+            return new SolicitudDeIngresoPedido(esCenabast, documentoVentaCenabast);
+        }
+    }
+}
