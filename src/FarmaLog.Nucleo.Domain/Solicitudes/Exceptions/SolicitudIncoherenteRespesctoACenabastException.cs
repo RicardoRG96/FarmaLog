744f50e45ac2644740dcdfba3e301ac24850bc5b
RicardoRG96 <ricardoretamal10@gmail.com>
2026-07-25 11:34:57 -0400

    add: SolicitudIncoherenteRespesctoACenabastException

---
+++ src/FarmaLog.Nucleo.Domain/Solicitudes/Exceptions/SolicitudIncoherenteRespesctoACenabastException.cs
@@ -0,0 +1,10 @@
+﻿using FarmaLog.Nucleo.Domain.Common;
+
+namespace FarmaLog.Nucleo.Domain.Solicitudes.Exceptions
+{
+    public class SolicitudIncoherenteRespesctoACenabastException : DomainException
+    {
+        public SolicitudIncoherenteRespesctoACenabastException(string message) 
+            : base(message) {}
+    }
+}
