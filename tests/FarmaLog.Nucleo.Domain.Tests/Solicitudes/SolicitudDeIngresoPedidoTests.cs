b621648e49c64be57447deb04a3056135e37cfc6
RicardoRG96 <ricardoretamal10@gmail.com>
2026-07-25 11:29:17 -0400

    add: first test SolicitudDeIngresoPedidoTests

---
+++ tests/FarmaLog.Nucleo.Domain.Tests/Solicitudes/SolicitudDeIngresoPedidoTests.cs
@@ -0,0 +1,14 @@
+﻿namespace FarmaLog.Nucleo.Domain.Tests.Solicitudes
+{
+    [TestClass]
+    public class SolicitudDeIngresoPedidoTests
+    {
+        [TestMethod]
+        public void SolicitudDeIngresoPedido_ShouldThrow_When_EsCenabastIsTrueButDocumentoVentaCenabastIsNull()
+        {
+            //Act + Assert
+            Assert.ThrowsExactly<SolicitudIncoherenteRespesctoACenabastException>(
+                () => SolicitudDeIngresoPedido.Create(true, null));
+        }
+    }
+}
