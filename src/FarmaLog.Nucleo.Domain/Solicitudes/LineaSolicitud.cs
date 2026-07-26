42848ae806c85a4a06704a47011e9586999a6601
RicardoRG96 <ricardoretamal10@gmail.com>
2026-07-26 08:48:13 -0400

    add: test LineaSolicitud sku whitespace

--- src/FarmaLog.Nucleo.Domain/Solicitudes/LineaSolicitud.cs
+++ src/FarmaLog.Nucleo.Domain/Solicitudes/LineaSolicitud.cs
@@ -19,7 +19,7 @@ namespace FarmaLog.Nucleo.Domain.Solicitudes
             if (cantidad <= 0)
                 throw new LineaSolicitudInvalidaException("La cantidad debe ser mayor a cero");
 
-            if (sku == "")
+            if (string.IsNullOrWhiteSpace(sku))
                 throw new LineaSolicitudInvalidaException("El SKU es obligatorio");
 
             return new LineaSolicitud(sku, cantidad);
--- tests/FarmaLog.Nucleo.Domain.Tests/Solicitudes/LineaSolicitudTests.cs
+++ tests/FarmaLog.Nucleo.Domain.Tests/Solicitudes/LineaSolicitudTests.cs
@@ -29,5 +29,13 @@ namespace FarmaLog.Nucleo.Domain.Tests.Solicitudes
             Assert.ThrowsExactly<LineaSolicitudInvalidaException>(
                 () => LineaSolicitud.Create("", 1));
         }
+
+        [TestMethod]
+        public void LineaSolicitud_ShouldThrow_When_SkuIsWhiteSpace()
+        {
+            //Act + Assert
+            Assert.ThrowsExactly<LineaSolicitudInvalidaException>(
+                () => LineaSolicitud.Create("  ", 1));
+        }
     }
 }
