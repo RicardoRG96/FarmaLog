df73c26fb9a5cb92e9587577c67d70567c9b8884
RicardoRG96 <ricardoretamal10@gmail.com>
2026-07-27 07:34:58 -0400

    add: test Solicitud ingreso construct with fecha entrega in the future

--- tests/FarmaLog.Nucleo.Domain.Tests/Solicitudes/SolicitudDeIngresoPedidoTests.cs
+++ tests/FarmaLog.Nucleo.Domain.Tests/Solicitudes/SolicitudDeIngresoPedidoTests.cs
@@ -197,5 +197,22 @@ namespace FarmaLog.Nucleo.Domain.Tests.Solicitudes
             //Assert
             Assert.AreEqual(fechaEntrega, solicitud.FechaEntregaSolicitada);
         }
+
+        [TestMethod]
+        public void SolicitudDeIngresoPedido_ShouldConstruct_When_FechaEntregaSolicitadaIsInTheFuture()
+        {
+            //Arrange
+            IReadOnlyCollection<LineaSolicitud> lineas = CrearLineas(1);
+            string observacion = new string('A', 100);
+            DateOnly hoy = new DateOnly(2026, 7, 27);
+            DateOnly fechaEntrega = new DateOnly(2026, 7, 28);
+
+            //Act
+            SolicitudDeIngresoPedido solicitud = SolicitudDeIngresoPedido.Create(
+                false, null, lineas, observacion, fechaEntrega, hoy);
+
+            //Assert
+            Assert.AreEqual(fechaEntrega, solicitud.FechaEntregaSolicitada);
+        }
     }
 }
