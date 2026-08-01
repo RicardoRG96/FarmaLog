2f273acb437bc557dfceb90060772358cc6757e2
RicardoRG96 <ricardoretamal10@gmail.com>
2026-08-01 11:56:33 -0400

    add RelojFijo

---
+++ tests/FarmaLog.Nucleo.Application.Tests/Doubles/RelojFijo.cs
@@ -0,0 +1,10 @@
+﻿using FarmaLog.Nucleo.Application.Ports;
+
+namespace FarmaLog.Nucleo.Application.Tests.Doubles
+{
+    public sealed class RelojFijo : IReloj
+    {
+        public RelojFijo(DateTime ahoraUtc) => AhoraUtc = ahoraUtc;
+        public DateTime AhoraUtc { get; }
+    }
+}
