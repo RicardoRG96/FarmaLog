e57f9222d97a0dbfc8e2ad468189c8505172316c
RicardoRG96 <ricardoretamal10@gmail.com>
2026-08-01 11:52:54 -0400

    add RepositorioDeSolicitudesEnMemoria

---
+++ tests/FarmaLog.Nucleo.Application.Tests/Doubles/RepositorioDeSolicitudesEnMemoria.cs
@@ -0,0 +1,18 @@
+﻿using FarmaLog.Nucleo.Application.Ports;
+using FarmaLog.Nucleo.Domain.Solicitudes;
+
+namespace FarmaLog.Nucleo.Application.Tests.Doubles
+{
+    public class RepositorioDeSolicitudesEnMemoria : IRepositorioDeSolicitudes
+    {
+        private readonly List<SolicitudDeIngresoPedido> _guardadas = new();
+
+        public IReadOnlyList<SolicitudDeIngresoPedido> Guardadas => _guardadas;
+
+        public Task<ResultadoGuardado> Guardar(SolicitudDeIngresoPedido solicitud, CancellationToken ct = default)
+        {
+            _guardadas.Add(solicitud);
+            return Task.FromResult(ResultadoGuardado.Guardada);
+        }
+    }
+}
