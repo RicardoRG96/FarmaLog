99e489257ba51fc44a42e3e409c48dda1854c019
RicardoRG96 <ricardoretamal10@gmail.com>
2026-08-01 11:40:03 -0400

    add: IRepositorioDeSolicitudes

---
+++ src/FarmaLog.Nucleo.Application/Ports/IRepositorioDeSolicitudes.cs
@@ -0,0 +1,9 @@
+﻿using FarmaLog.Nucleo.Domain.Solicitudes;
+
+namespace FarmaLog.Nucleo.Application.Ports
+{
+    public interface IRepositorioDeSolicitudes
+    {
+        Task<ResultadoGuardado> Guardar(SolicitudDeIngresoPedido solicitud, CancellationToken ct = default);
+    }
+}
