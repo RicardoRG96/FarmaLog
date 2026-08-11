3c8792f4b9c700ffdf82f4b04a259eb62faf47cc
RicardoRG96 <ricardoretamal10@gmail.com>
2026-08-11 16:54:35 -0400

    refactor to LineaMessage

--- src/FarmaLog.Nucleo.Infrastructure/Messaging/LineaMessage.cs
+++ src/FarmaLog.Nucleo.Infrastructure/Messaging/LineaMessage.cs
@@ -1,7 +1,5 @@
 ﻿namespace FarmaLog.Nucleo.Infrastructure.Messaging
 {
     public sealed record LineaMessage(
-        string Sku, int 
-        
-        Cantidad, string EstadoInventario, string? Lote);
+        string Sku, int Cantidad, string EstadoInventario, string? Lote);
 }
