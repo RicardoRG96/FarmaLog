6923b6ad8f48df753c9a55a80f79845e8461f2fc
RicardoRG96 <ricardoretamal10@gmail.com>
2026-08-11 16:49:17 -0400

    add GeneradorDeIdentificadores

---
+++ src/FarmaLog.Nucleo.Infrastructure/Identity/GeneradorDeIdentificadores.cs
@@ -0,0 +1,9 @@
+﻿using FarmaLog.Nucleo.Application.Ports;
+
+namespace FarmaLog.Nucleo.Infrastructure.Identity
+{
+    public sealed class GeneradorDeIdentificadores : IGeneradorDeIdentificadores
+    {
+        public Guid Nuevo() => Guid.CreateVersion7();
+    }
+}
