11829cec5f44144db9193bb5c8e7b36cbcc5dff5
RicardoRG96 <ricardoretamal10@gmail.com>
2026-08-22 14:07:25 -0400

    add: ErrorDeValidacion to Ingesta

---
+++ src/FarmaLog.Ingesta/ErrorDeValidacion.cs
@@ -0,0 +1,12 @@
+﻿namespace FarmaLog.Ingesta
+{
+    internal sealed record ErrorDeValidacion(
+        string Problema,
+        string ValorOfensor,
+        string Ubicacion,
+        string ComoCorregir)
+    {
+        public override string ToString() =>
+            $"{Problema}: '{ValorOfensor}' en el pedido {Ubicacion}. {ComoCorregir}";
+    }
+}
