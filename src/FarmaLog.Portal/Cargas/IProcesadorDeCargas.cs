eb19b7dd761551ad7d1a3199f4170e83a6076d8e
RicardoRG96 <ricardoretamal10@gmail.com>
2026-08-22 07:53:29 -0400

    add IProcesadorDeCargas

---
+++ src/FarmaLog.Portal/Cargas/IProcesadorDeCargas.cs
@@ -0,0 +1,7 @@
+﻿namespace FarmaLog.Portal.Cargas
+{
+    public interface IProcesadorDeCargas
+    {
+        Task<ResultadoDeCarga> ProcesarAsync(ArchivoDeCarga archivo, CancellationToken ct);
+    }
+}
