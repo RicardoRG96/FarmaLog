c591c9366552ff13383984db3463072efb569072
RicardoRG96 <ricardoretamal10@gmail.com>
2026-08-22 08:06:46 -0400

    add ClienteHttpDeIngesta to Portal

---
+++ src/FarmaLog.Portal/Cargas/ClienteHttpDeIngesta.cs
@@ -0,0 +1,22 @@
+﻿using System.Net.Http.Headers;
+
+namespace FarmaLog.Portal.Cargas
+{
+    internal sealed class ClienteHttpDeIngesta(HttpClient http) : IProcesadorDeCargas
+    {
+        public async Task<ResultadoDeCarga> ProcesarAsync(ArchivoDeCarga archivo, CancellationToken ct)
+        {
+            using MultipartFormDataContent content = new();
+            using StreamContent streamContent = new(archivo.Contenido);
+            streamContent.Headers.ContentType = new MediaTypeHeaderValue("application/octet-stream");
+
+            content.Add(streamContent, "archivo", archivo.Nombre);
+
+            using HttpResponseMessage response = await http.PostAsync("/cargas", content, ct);
+            response.EnsureSuccessStatusCode();
+
+            return await response.Content.ReadFromJsonAsync<ResultadoDeCarga>(ct)
+                ?? throw new InvalidOperationException("Ingesta respondió un cuerpo vacío");
+        }
+    }
+}
